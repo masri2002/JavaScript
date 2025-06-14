e13f1feb483f5b3477e6c6bd6c6af98d03905d55
Ahmad Al-Masri <ahmadmmasrri1@gmail.com>
2025-06-14 19:54:48 +0300

    generateYears example

--- function.js
+++ function.js
@@ -5,20 +5,31 @@
 
 // console.log(typeof console.log);
 
-function sayHello(firstName , age ) {
-    if (age<20) {
-        console.log('you are young , this app is not for you'+ ' ' + firstName);
-    }else {
-            console.log(`hello ${firstName} you are ${age} years old`);
+// function sayHello(firstName , age ) {
+//     if (age<20) {
+//         console.log('you are young , this app is not for you'+ ' ' + firstName);
+//     }else {
+//             console.log(`hello ${firstName} you are ${age} years old`);
+//     }
+// }
+
+// sayHello('ahmad' , 20);
+// sayHello('saleem' , 12);
+// sayHello('ali' , 30);
+// sayHello('sara' , 15);
+// sayHello('shahed' , 25);
+// sayHello('sara' , 18);
+
+// sayHello('sara' , 18);
+
+
+function gernerateYears(startYear, endYear ,excluedYear) {
+    for (let i = startYear; i <= endYear; i++) {
+        if (i === excluedYear) {
+            continue;
+        }
+        console.log(i);
     }
 }
 
-sayHello('ahmad' , 20);
-sayHello('saleem' , 12);
-sayHello('ali' , 30);
-sayHello('sara' , 15);
-sayHello('shahed' , 25);
-sayHello('sara' , 18);
-
-sayHello('sara' , 18);
-
+gernerateYears(2000, 2023 , 2010);
