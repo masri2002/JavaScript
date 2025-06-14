8b5efbc6a060b15c1c99b1ed7acdb34fe66b4120
ahmad masri <ahmad.almasri@progressoft.com>
2025-06-14 19:24:23 +0300

    learning function in js

---
+++ function.js
@@ -0,0 +1,20 @@
+/*
+  Fuction
+
+*/
+
+// console.log(typeof console.log);
+
+function sayHello(firstName , age ) {
+    if (age<20) {
+        console.log('you are young , this app is not for you'+ ' ' + firstName);
+    }else {
+            console.log(`hello ${firstName} you are ${age} years old`);
+    }
+}
+
+sayHello('ahmad' , 20);
+sayHello('saleem' , 12);
+sayHello('ali' , 30);
+sayHello('sara' , 15);
+sayHello('shahed' , 25);
